3883df59c05dc19def852e59849ec76a79d2264b
sepidebri <sepide.bri@gmail.com>
2022-09-14 18:23:55 +0430

    :fire:first initial :fire:

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        sonil-graphic
+      </div>
+    );
+  }
+}
+
+export default App;
